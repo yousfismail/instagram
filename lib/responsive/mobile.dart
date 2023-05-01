d522196006953dd53237a7f6231b1f5b854802e6
yousfismail <yousfismail1200@gmail.com>
2023-05-01 08:36:39 +0300

    create widget to mobile

---
+++ lib/responsive/mobile.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_app/screens/add_post.dart';
+import 'package:instagram_app/screens/home.dart';
+import 'package:instagram_app/screens/profile.dart';
+import 'package:instagram_app/screens/search.dart';
+import 'package:instagram_app/shered/colors.dart';
+
+class MobileScreen extends StatefulWidget {
+  const MobileScreen({super.key});
+
+  @override
+  State<MobileScreen> createState() => _MobileScreenState();
+}
+
+class _MobileScreenState extends State<MobileScreen> {
+  final PageController _pageController = PageController();
+
+  int currentPage = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CupertinoTabBar(
+          backgroundColor: mobileBackgroundColor,
+          onTap: (index) {
+            _pageController.jumpToPage(index);
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                  color: currentPage == 0 ? primaryColor : secondaryColor,
+                ),
+                label: ""),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.search,
+                  color: currentPage == 1 ? primaryColor : secondaryColor,
+                ),
+                label: ""),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.add_circle,
+                  color: currentPage == 2 ? primaryColor : secondaryColor,
+                ),
+                label: ""),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.favorite,
+                  color: currentPage == 3 ? primaryColor : secondaryColor,
+                ),
+                label: ""),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person,
+                  color: currentPage == 4 ? primaryColor : secondaryColor,
+                ),
+                label: ""),
+          ]),
+      body: PageView(
+        onPageChanged: (index) {
+          print("------- $index");
+        },
+        physics: NeverScrollableScrollPhysics(),
+        controller: _pageController,
+        children: [
+          Home(),
+          Search(),
+          AddPost(),
+          Center(child: Text("Love u ♥")),
+          Profile(),
+        ],
+      ),
+    );
+  }
+}
