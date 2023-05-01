b4cff3181892a52c1af300931d713d125b452142
yousfismail <yousfismail1200@gmail.com>
2023-05-01 08:38:39 +0300

    add LayoutBuilder

--- lib/responsive/responsive.dart
+++ lib/responsive/responsive.dart
@@ -1,1 +1,25 @@
 import 'package:flutter/material.dart';
+class Responsive extends StatefulWidget {
+  final myMobileScreen;
+  final myWebScreen;
+
+  const Responsive(
+      {Key? key, required this.myMobileScreen, required this.myWebScreen})
+      : super(key: key);
+
+  @override
+  State<Responsive> createState() => _ResponsiveState();
+}
+
+class _ResponsiveState extends State<Responsive> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (BuildContext, BoxConstraints) {
+      if (BoxConstraints.maxWidth > 600) {
+        return widget.myWebScreen;
+      } else {
+        return widget.myMobileScreen;
+      }
+    });
+  }
+}
