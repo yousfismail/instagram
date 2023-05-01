d6cf381a9f6ab0ba21a2b577862070c1f5a8ca01
yousfismail <yousfismail1200@gmail.com>
2023-05-01 08:43:56 +0300

    add primaryColor

--- lib/shered/colors.dart
+++ lib/shered/colors.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
 const webBackgroundColor = Color.fromRGBO(32, 33, 36, 1);
+const primaryColor = Colors.white;
+
