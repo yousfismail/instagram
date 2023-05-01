2fb6b43a9c272c7b2a58e6b9099156d47238cd86
yousfismail <yousfismail1200@gmail.com>
2023-05-01 08:47:48 +0300

    code add post

---
+++ lib/screens/add_post.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_app/shered/colors.dart';
+
+class AddPost extends StatefulWidget {
+  const AddPost({Key? key}) : super(key: key);
+
+  @override
+  State<AddPost> createState() => _AddPostState();
+}
+
+class _AddPostState extends State<AddPost> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: mobileBackgroundColor,
+      body: Center(
+        child: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.upload,
+              size: 55,
+            )),
+      ),
+    );
+  }
+}
